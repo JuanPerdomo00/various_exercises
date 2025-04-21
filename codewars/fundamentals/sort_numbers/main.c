8f367f3b3aeb25d60607bac757f37dfb714c4085
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-04-21 16:33:10 -0500

    Resolve: Ok

--- codewars/fundamentals/sort_numbers/main.c
+++ codewars/fundamentals/sort_numbers/main.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
-
 void sort_ascending (size_t length, int array[length]) {
     for (int i = 0; i < length; i++) {
-        for (int j = i; j < length - i - 1; j++) {
+        for (int j = 0; j < length - i - 1; j++) {
             if (array[j] > array[j+1]) {
                 int aux = array[j];
                 array[j] = array[j+1];
                 array[j + 1] = aux;
             }
         }
-
-        printf("%d\n", array[i]);
-    
     }
 
+    for (int i = 0; i < length; i++) {
+        printf("%d\n", array[i]);
+    }
 }
 
 int main() {
@@ -22,14 +21,25 @@ int main() {
     int array[5] = {1,2,10,50,5};
     int array_2[6] = {0,3,1,4,2,5};
     int array_3[10] = {1,0,3,6,9,11,10,5,22,100};
+    int array_4[5] = {-30, -8, 3, 16, 22};
+
+    printf("=================\n");
     printf("Arr [1,2,10,50,5]\n");
     sort_ascending(5, array);
+
     printf("=================\n");
     printf("Arr [0,3,1,4,2,5]\n");
     sort_ascending(6, array_2);
+
     printf("=================\n");
     printf("Arr [1,0,3,6,9,11,10,5,22,100]\n");
     sort_ascending(10, array_3);
+
     printf("=================\n");
+    printf("Arr [-30, -8, 3,16, 22]\n");
+    sort_ascending(5, array_4);
+
+    printf("================\n");
     return 0;
 }
+
