b0199556f36aa3c613c25667dc8b6985c028a991
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-04-21 16:54:21 -0500

    Resolve: Hackerrand ok

---
+++ hackerrank/birthdayCakeCandles/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def birthdayCakeCandles(candles: list[int]):
+    from collections import Counter
+    return max(Counter(candles).values())
+    #return max(list(dict((i, sorted(candles).count(i)) for i in candles).values()))
+
+
+if __name__ == "__main__":
+    print(birthdayCakeCandles([3, 2, 1, 3]))
+    print(birthdayCakeCandles([4, 4, 1, 3]))
