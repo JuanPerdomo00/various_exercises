af5b73ae79122256fcfe6a6c872baa95945a740f
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-09-25 14:03:09 -0500

    Update: Formated

--- codeforces/watermelon/main.cpp
+++ codeforces/watermelon/main.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 
-
-
-using namespace std; 
-
+using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int n;
-    cin >> n;
-    string yes_or_no = (n % 2 == 0) && (n > 2) ? "YES" : "NO";
-    cout << yes_or_no;
-    return 0;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  string yes_or_no = (n % 2 == 0) && (n > 2) ? "YES" : "NO";
+  cout << yes_or_no;
+  return 0;
 }
