5483d04abfe9738be52ed8c00fe7fa78c573ef5b
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-09-25 14:01:39 -0500

    Add: Solve a easy

---
+++ codeforces/watermelon/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+
+
+using namespace std; 
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    string yes_or_no = (n % 2 == 0) && (n > 2) ? "YES" : "NO";
+    cout << yes_or_no;
+    return 0;
+}
