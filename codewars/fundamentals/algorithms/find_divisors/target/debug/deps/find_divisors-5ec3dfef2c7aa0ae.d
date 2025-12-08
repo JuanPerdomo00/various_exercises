c8e7786742361d0cc1a7dd452aae83000c13259f
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-12-08 10:23:29 -0500

    Remove: trash

--- codewars/fundamentals/algorithms/find_divisors/target/debug/deps/find_divisors-5ec3dfef2c7aa0ae.d
+++
@@ -1,5 +0,0 @@
-/home/jakepys/Escritorio/various_exercises/codewars/fundamentals/algorithms/find_divisors/target/debug/deps/find_divisors-5ec3dfef2c7aa0ae.d: src/main.rs
-
-/home/jakepys/Escritorio/various_exercises/codewars/fundamentals/algorithms/find_divisors/target/debug/deps/libfind_divisors-5ec3dfef2c7aa0ae.rmeta: src/main.rs
-
-src/main.rs:
--- codewars/fundamentals/algorithms/find_divisors/target/debug/deps/find_divisors-daa6628f2d2f5231.d
+++
@@ -1,5 +0,0 @@
-/home/jakepys/Escritorio/various_exercises/codewars/fundamentals/algorithms/find_divisors/target/debug/deps/find_divisors-daa6628f2d2f5231.d: src/main.rs
-
-/home/jakepys/Escritorio/various_exercises/codewars/fundamentals/algorithms/find_divisors/target/debug/deps/libfind_divisors-daa6628f2d2f5231.rmeta: src/main.rs
-
-src/main.rs:
